f9c0128614156b88d69545fac7c579a4a2046f60
Vindaar <basti90@gmail.com>
2022-06-08 19:38:53 +0200

    [constants] add `m_u` (`M_u / N_A`) and rename `k` -> `k_B`

--- src/unchained/constants.nim
+++ src/unchained/constants.nim
@@ -12,10 +12,11 @@ let
   m_e_c2* = 0.510998928.MeV
   N_A* = 6.02214076e23.mol⁻¹
   M_u* = 0.99999999965e-3.kg•mol⁻¹
+  m_u* = M_u / N_A
   m_μ_eV* = 105.6583755e6.eV # / c²
   m_μ* = 1.883531627e-28.kg # 105.6583755e3 # MeV / c²
   π* = PI
   r_e* = e*e / (4 * π * ε_0 * m_e * c * c) # classical electron radius
   #K = 4 * π * N_A * r_e * r_e * m_e_c2 * (100.0^2)# [MeV mol⁻¹ cm²]
-  k* = 1.380649e-23.Joule•Kelvin⁻¹
+  k_B* = 1.380649e-23.Joule•Kelvin⁻¹
   hp* = 6.62607015e-34.Joule•Hertz⁻¹
