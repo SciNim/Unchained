a9e79df2ce9d2d827c436177dea53c98f8cc4c33
Vindaar <basti90@gmail.com>
2022-07-26 17:12:24 +0200

    move SI unit system definition from main file to separate module

--- src/unchained.nim
+++ src/unchained.nim
@@ -2,5 +2,10 @@ import unchained / [units, constants, utils, core_types]
 
 export core_types
 export units
+# import and export the default unit system
+import unchained / si_units
+export si_units
+
+# import & export units defined on top of
+import unchained / constants
 export constants
-export utils
