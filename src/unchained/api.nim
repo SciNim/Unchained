6fccb665d9eff521b41359f36b6d043d768b2354
Vindaar <basti90@gmail.com>
2022-07-26 17:07:38 +0200

    add `api.nim` and `ct_api.nim` helper modules for easier imports
    `api.nim` imports and exports the whole user facing API. `ct_api` as
    the name suggests provides the compile time API to declare quantities
    and units.

---
+++ src/unchained/api.nim
@@ -0,0 +1,15 @@
+## This file provides the basic API to handle units and quantities
+##
+## You can import this, if you intend to implement your own custom unit system.
+##
+## import unchained / api # the user facing macros & procedures dealing with units
+## import unchained / ct_api # the CT logic to define quantities and units
+## declareQuantity: ...
+## declareUnits: ...
+
+# import the core types, API & utils
+import unchained / [utils, core_types, units]
+
+export core_types
+export units
+export utils
