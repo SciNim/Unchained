95f21876f602d5fa8b89baf90e124e0649d05ca5
Vindaar <basti90@gmail.com>
2022-11-16 18:39:59 +0100

    extend `^` for static integer exponents for < 2 & negative exponents

--- src/unchained/utils.nim
+++ src/unchained/utils.nim
@@ -1,16 +1,35 @@
 import std / [macros]
 
-macro `^`*(x: untyped, num: static int): untyped =
+macro `^`*(x: typed, num: static int): untyped =
   ## general purpose power using `^` for integers, which works for any
   ## type by rewriting to a product of `*`.
-  result = nnkInfix.newTree(ident"*")
+  ##
+  ## For the special cases of -1, 0, 1 we simply rewrite to the correct
+  ## result. Negative powers are written as `1 / x^p`
+  if num == 0:
+    result = quote do:
+      `x` * typeof(`x`)(0.0)
+  elif num == 1:
+    result = x
+  elif num == -1:
+    ## Assume that the type supports addition by a float!
+    result = quote do:
+      1.0 / `x`
+  else:
+    result = nnkInfix.newTree(ident"*")
 
-  proc addInfix(n, x: NimNode, num: int) =
-    var it = n
-    if num > 0:
-      it.add nnkInfix.newTree(ident"*")
-      it[1].addInfix(x, num - 1)
-    while it.len < 3:
-      it.add x
+    proc addInfix(n, x: NimNode, num: int) =
+      var it = n
+      if num > 0:
+        it.add nnkInfix.newTree(ident"*")
+        it[1].addInfix(x, num - 1)
+      while it.len < 3:
+        it.add x
 
-  result.addInfix(x, num - 2)
+    result.addInfix(x, abs(num) - 2)
+
+    # invert if num is negative
+    if num < -1:
+      ## Assume that the type supports addition by a float!
+      result = quote do:
+        1.0 / (`result`)
