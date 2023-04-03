09da7ea78011bf09ed242453896aa81cd58a9ca2
Vindaar <basti90@gmail.com>
2023-04-03 16:25:52 +0200

    add (optional) submodule to provide support for units in a cligen proj

---
+++ src/unchained/cligenParseUnits.nim
@@ -0,0 +1,22 @@
+import cligen/argcvt
+import unchained
+proc argParse*[T: SomeUnit](dst: var T, dfl: T,
+                           a: var ArgcvtParams): bool =
+  try:
+    let aStr = a.val
+    var tmp: T
+    let unitName = unitOf(tmp)
+    if aStr.endsWith(unitName):
+      proc removeSuffix(s, p: string): string =
+        result = s
+        result.removeSuffix(p)
+      dst = aStr.removeSuffix(unitName).strip.parseFloat.T
+    else:
+      dst = aStr.strip.parseFloat.T
+    result = true
+  except:
+    result = false
+
+proc argHelp*[T: SomeUnit](dfl: T; a: var ArgcvtParams): seq[string] =
+  var tmp: T
+  result = @[ a.argKeys, unitOf(tmp), $dfl ]
