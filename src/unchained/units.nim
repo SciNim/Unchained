9024bc2e2736391463f2252ab930c427b6094954
Vindaar <basti90@gmail.com>
2025-11-20 11:31:19 +0100

    add `simplify` macro, which returns unit in base SI units

--- src/unchained/units.nim
+++ src/unchained/units.nim
@@ -594,3 +594,13 @@ macro toNaturalUnit*[T: SomeUnit](t: T): untyped =
   result = quote do:
     defUnit(`resType`)
     `resType`(`t`.FloatType * `scale`)
+
+macro simplify*[T: SomeUnit](t: T): untyped =
+  ## Returns the simplest form in SI base units of the given input.
+  var typ = t.parseDefinedUnit()
+  let resType = typ.flatten().simplify()
+  let scale = typ.toBaseTypeScale() / resType.toBaseTypeScale()
+  let resTypeNim = resType.toNimType()
+  result = quote do:
+    defUnit(`resTypeNim`)
+    `resTypeNim`(`t`.FloatType * `scale`)
